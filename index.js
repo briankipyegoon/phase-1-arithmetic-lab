977284fde89d520c42af28c360aa6a9a116bf41c
briankipyegon <kipyegonbrian201@gmail.com>
2024-10-07 16:58:27 +0300

    'Arithmetic lab done'

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+let num1 = 31;
+let num2 = 2;
+let multiply = num1*num2;
 
-// Write your code here
+let random = 9;
+
+let num3 = 16;
+let num4 =4;
+let mod = num3/num4;
+
+let max = Math.max(13, 14, 20, 7);
